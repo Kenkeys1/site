2f9e4a9b5c3724a3765fd70f0d84a8024170e975
Your Name <your-email@example.com>
2026-07-22 00:35:42 +0300

    hard

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let inventory = [];
 // Initialize the site
 async function init() {
     try {
-        const response = await fetch('products.json');
+        const response = await fetch('/api/products');
         inventory = await response.json();
         displayProducts(inventory);
     } catch (err) {
